02a720ba26aa433fdb5ecd543c44038282a90e94
debutler266 <32465329+debutler266@users.noreply.github.com>
2021-12-24 19:49:35 -0500

    finished styling navigation, also styled the skills section & about sections

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ if(navToggle){
     navMenu.classList.add('show-menu')
   });
 };
+
+//Menu hidden when CLOSED
+if(navClose){
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  });
+};
+
+//Remove the Mobile Menu
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
