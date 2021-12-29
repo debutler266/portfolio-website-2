ad53ec42cf17a7dd37130e9ab4a1ded619b065f9
debutler266 <32465329+debutler266@users.noreply.github.com>
2021-12-29 18:20:17 -0500

    styled 7 added full fuctionality to services section + services modal I created using CSS/JS

--- main.js
+++ main.js
@@ -68,3 +68,29 @@ const tabs = document.querySelectorAll('[data-target]'),
       tab.classList.add('qualification_active')
     });
   });
+
+
+
+  //Services Modal
+  const modalViews = document.querySelectorAll('.services_modal'),
+  modalBtns = document.querySelectorAll('.services_button'),
+  modalCloses = document.querySelectorAll('.services_modal-close')
+
+  let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+  }
+
+  modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+      modal(i)
+    })
+  })
+
+  //close services modal
+  modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+      modalViews.forEach((modalView) => {
+        modalView.classList.remove('active-modal')
+      })
+    })
+  })
